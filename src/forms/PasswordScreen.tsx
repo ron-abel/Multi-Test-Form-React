6b422975d7852f5aa9b27ee9fe5587959d7a4c13
RonAbel <ronabel.develops@gmail.com>
2023-10-26 22:56:34 +0500

    First Commit

--- src/forms/PasswordScreen.tsx
+++ src/forms/PasswordScreen.tsx
@@ -1,30 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import '../App.css';
 
-export default function PasswordScreen(props:any) {
-    interface passwordFields{
+export default function PasswordScreen(props: any) {
+    interface passwordFields {
 
         password: string,
-        repeatPassword:string,
-        
+        repeatPassword: string,
+
     };
-    const [pass,setPass] = useState<passwordFields>({password:"",repeatPassword:""});
-    const [enablePassword,setEnablePassword] = useState<boolean>(false);
+    const [pass, setPass] = useState<passwordFields>({ password: "", repeatPassword: "" });
+    const [enablePassword, setEnablePassword] = useState<boolean>(false);
 
     useEffect(() => {
-        if(pass.password != '' && pass.repeatPassword != ''){
+        if (pass.password != '' && pass.repeatPassword != '') {
 
-            if(pass.password === pass.repeatPassword){
+            if (pass.password === pass.repeatPassword) {
                 setEnablePassword(true);
             }
-            else{
+            else {
                 setEnablePassword(false);
             }
-            
         }
-    },[pass.password,pass.repeatPassword]);
+    }, [pass.password, pass.repeatPassword]);
 
-    function gotoReview(){
+    function gotoReview() {
         props.func(pass);
     }
 
@@ -33,17 +32,17 @@ export default function PasswordScreen(props:any) {
             <form action=''>
                 <div className='form-group pb-4'>
                     <label className='alignLeft'>Password</label>
-                    <input type='password' id='disabledTextInput' className='form-control border-0 form-control-sm rounded-0' placeholder='Input password' value={pass.password} onChange={event => {setPass({...pass, password:event.target.value})}}/>
+                    <input type='password' id='disabledTextInput' className='form-control border-0 form-control-sm rounded-0' placeholder='Input password' value={pass.password} onChange={event => { setPass({ ...pass, password: event.target.value }) }} />
                 </div>
 
                 <div className='form-group pb-4'>
                     <label className='alignLeft'>Repeat Password</label>
-                    <input type='password' className='form-control border-0 rounded-0 form-control-sm' placeholder='Repeat password' value={pass.repeatPassword} onChange={event => {setPass({...pass,repeatPassword:event.target.value})}}/>
+                    <input type='password' className='form-control border-0 rounded-0 form-control-sm' placeholder='Repeat password' value={pass.repeatPassword} onChange={event => { setPass({ ...pass, repeatPassword: event.target.value }) }} />
                 </div>
 
-                
+
                 <div className='form-group pb-3'>
-                    <button onClick={gotoReview} type="button" className={!enablePassword === true? 'btn w-100 btn-lg':'btn w-100 bg-white text-black btn-lg'} disabled={!enablePassword}>Continue</button>
+                    <button onClick={gotoReview} type="button" className={!enablePassword === true ? 'btn w-100 btn-lg' : 'btn w-100 bg-white text-black btn-lg'} disabled={!enablePassword}>Continue</button>
                 </div>
 
             </form>
